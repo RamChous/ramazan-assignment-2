75b6dfb21eb702fb2f7c4b0245d0e1fd17554cea
Ramazan <rahmettularamazan@gmail.com>
2025-04-13 21:07:39 +0500

    testing MyStack

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,21 @@ import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        MyList<Integer> array = new MyLinkedList<>();
-        array.add(23);
-        array.add(14);
-        array.add(34);
-        array.add(14);
-        array.add(99);
-        array.add(1);
-        for (int i : array)
-            System.out.println(i);
-        System.out.println("//////////");
-        array.sort(Comparator.naturalOrder());
-        for (int i : array)
-            System.out.println(i);
+        MyStack<Integer> stack = new MyStack<>();
+
+        System.out.println(stack.empty());
+
+        stack.push(23);
+        stack.push(14);
+        stack.push(99);
+        stack.push(1);
+
+        System.out.println(stack.empty());
+
+        System.out.println(stack.pop());
+
+        System.out.println(stack.peek());
+
+        System.out.println(stack.size());
     }
 }
