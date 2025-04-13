d0d565b0aab6406bf01bb3314d05431ac30f552f
Ramazan <rahmettularamazan@gmail.com>
2025-04-13 19:27:53 +0500

    creating node method, all adds, all gets, set, size methods and Iterator.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
-import java.util.Comparator;
-
 public class Main {
     public static void main(String[] args) {
-        MyList<Integer> array = new MyArrayList<>();
-        array.add(33);
-        array.add(16);
+        MyList<Integer> array = new MyLinkedList<>();
         array.add(23);
-        array.add(7);
-        array.add(23);
-        array.add(99);
+        array.add(14);
+        array.addLast(100);
+        array.addFirst(33);
         for (int i = 0; i < array.size(); i++)
             System.out.println(array.get(i));
-        System.out.println("/////");
-        array.sort(Comparator.naturalOrder());
-        for (int i = 0; i < array.size(); i++)
-            System.out.println(array.get(i));
+        System.out.println("///////");
+        array.add(2, 200);
+        for (int i : array)
+            System.out.println(i);
+        System.out.println("//////");
+        System.out.println(array.getFirst());
+        System.out.println(array.getLast());
+        System.out.println("//////");
+        array.addFirst(1);
+        array.addLast(9999);
+        array.set(3, 2345);
+        for (int i : array)
+            System.out.println(i);
     }
 }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -2,44 +2,102 @@ import java.util.Comparator;
 import java.util.Iterator;
 
 public class MyLinkedList<T> implements MyList<T>{
+
+    private MyNode<T> head;
+    private MyNode<T> tail;
+    private int size;
+
+    public MyLinkedList(){}
+
+    private static class MyNode<E>{
+        E data;
+        MyNode<E> next;
+        MyNode<E> prev;
+        MyNode(E data, MyNode<E> prev, MyNode<E> next){
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private MyNode<T> node(int index){
+        MyNode<T> current;
+        if (index < size / 2){
+            current = head;
+            for (int i = 0; i < index; i++)
+                current = current.next;
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--)
+                current = current.prev;
+        }
+        return current;
+    }
+
     @Override
     public void add(T item) {
-
+        addLast(item);
     }
 
     @Override
     public void set(int index, T item) {
-
+        MyNode<T> newNode = node(index);
+        newNode.data = item;
     }
 
     @Override
     public void add(int index, T item) {
-
+        if (index == 0) {
+            addFirst(item);
+        } else if (index == size) {
+            addLast(item);
+        } else {
+            MyNode<T> next = node(index);
+            MyNode<T> prev = next.prev;
+            MyNode<T> newNode = new MyNode<>(item, prev, next);
+            newNode.next = next;
+            newNode.prev = prev;
+            prev.next = newNode;
+            next.prev = newNode;
+            size++;
+        }
     }
 
     @Override
     public void addFirst(T item) {
-
+        MyNode<T> h = head;
+        MyNode<T> newNode = new MyNode<>(item, null, h);
+        head = newNode;
+        if (h == null)
+            tail = newNode;
+        else h.prev = newNode;
+        size++;
     }
 
     @Override
-    public void addLast(T Item) {
-
+    public void addLast(T item) {
+        MyNode<T> t = tail;
+        MyNode<T> newNode = new MyNode<>(item, t, null);
+        tail = newNode;
+        if (t == null)
+            head = newNode;
+        else t.next = newNode;
+        size++;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        return node(index).data;
     }
 
     @Override
     public T getFirst() {
-        return null;
+        return head.data;
     }
 
     @Override
     public T getLast() {
-        return null;
+        return tail.data;
     }
 
     @Override
@@ -89,11 +147,26 @@ public class MyLinkedList<T> implements MyList<T>{
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public Iterator iterator() {
-        return null;
+        return new MyIterator();
+    }
+    private class MyIterator implements Iterator<T>{
+        MyNode<T> cursor = head;
+
+        @Override
+        public boolean hasNext() {
+            return cursor != null;
+        }
+
+        @Override
+        public T next() {
+            T nextItem = cursor.data;
+            cursor = cursor.next;
+            return nextItem;
+        }
     }
 }
